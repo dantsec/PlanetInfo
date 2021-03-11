962cce30e8ab1c1235a7c13d37f6c8668422eae8
/Dantalion <72806462+Dantalion-dev@users.noreply.github.com>
2021-03-11 13:34:04 -0300

    Update run.py

--- packages/run.py
+++ packages/run.py
@@ -1,11 +1,9 @@
 def run():
   from packages.classeA import Astronomy
   from packages.classeB import sargs
-  from packages.classeB import request
 
   args = sargs()
   Astronomy = Astronomy(args)
   url  = Astronomy.planet_info()
   code = Astronomy.request(url)
   Astronomy.filter(code)
-
